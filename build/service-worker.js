16fe0c9625070ba147fe4ab43640da6aeb187e9e
Alexandre Vieira <av.souza2018@gmail.com>
2020-03-05 13:15:30 -0300

    updated

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/web/precache-manifest.19d21fcfcc5c3f52240a5fd3c3c6ffa7.js"
+  "/web/precache-manifest.42545eb3b24e156598788d16b89ce4be.js"
 );
 
 self.addEventListener('message', (event) => {
