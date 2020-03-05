783679291cb1325493d9a397fab322222b6cc8d0
Alexandre Vieira <av.souza2018@gmail.com>
2020-03-05 12:45:36 -0300

    Updates

---
+++ precache-manifest.19d21fcfcc5c3f52240a5fd3c3c6ffa7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0695deffc852e2b68be489847b6f666f",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "4133580b8094d222bf63",
+    "url": "/web/static/css/main.5d757d2b.chunk.css"
+  },
+  {
+    "revision": "2c98c82f5e04c7460668",
+    "url": "/web/static/js/2.471167b6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/web/static/js/2.471167b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4133580b8094d222bf63",
+    "url": "/web/static/js/main.217b471d.chunk.js"
+  },
+  {
+    "revision": "cab7086af34e62502fc8",
+    "url": "/web/static/js/runtime-main.eaef502f.js"
+  },
+  {
+    "revision": "84f56495c52e333d3c00d17af1bc5706",
+    "url": "/web/static/media/Logo.84f56495.png"
+  }
+]);
